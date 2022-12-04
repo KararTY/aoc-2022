ce61ba41534a886501fcec082b71e8991dfeb99d
Karar Al-Remahy <alremahykarar@ymail.com>
2022-12-04 01:09:22 +0100

    Day 3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod day1;
 mod day2;
+mod day3;
 
-use crate::{day1::day_one, day2::day_two};
+use crate::{day1::day_one, day2::day_two, day3::day_three};
 
 fn main() {
     println!("Hello, world!");
 
     day_one::run();
     day_two::run();
+		day_three::run();
 }
